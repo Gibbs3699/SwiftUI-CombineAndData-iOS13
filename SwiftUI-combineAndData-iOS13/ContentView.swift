88f30e682234ffd4a47e20a286ae919b58bca951
Gibbs3699 <gift_kamonpan@hotmail.com>
2021-11-28 22:05:07 +0700

    Initial Commit

---
+++ SwiftUI-combineAndData-iOS13/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-combineAndData-iOS13
+//
+//  Created by admin on 28/11/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-combineAndData-iOS13/SwiftUI_combineAndData_iOS13App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_combineAndData_iOS13App.swift
+//  SwiftUI-combineAndData-iOS13
+//
+//  Created by admin on 28/11/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_combineAndData_iOS13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
